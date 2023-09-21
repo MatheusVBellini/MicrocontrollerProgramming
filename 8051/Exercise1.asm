b60e17fff0c1264b321995a62e231f10008bf6bf
Matheus Violaro Bellini <matheus.bellini02@gmail.com>
2023-09-21 11:15:55 -0300

    exercicio1 done

--- 8051/Exercise1.asm
+++ 8051/Exercise1.asm
@@ -1,16 +1,16 @@
 org 00h
 
 main:
-	MOV  ACC,  #0Ah   ; move 0Ah diretamente para ACC
-	MOV  ACC,  #00h   ; move 00h diretamente para ACC
-	MOV  R0,   #0Bh   ; move 0Bh para R0 do banco 0
-	MOV  B,    #0Ch   ; move 0Ch para o registrador C
-	MOV  7Fh,  P1     ; move a porta P1 para o enderco 7Fh
-	SETB PSW.3        ; seleciona o banco 1
-	MOV  R0,   7Fh    ; escreve conteudo de 7Fh em R0 do banco 1
-	MOV  7Eh,  R0     ; escreve R0 (banco 1) em 7Eh
-	MOV  R1,   #7Eh   ; R1 ponteiro para 7Eh
-	MOV  ACC,  @R1    ; move conteudo no endereco R1 para ACC
-	MOV  DPTR, #9A5Bh ; move 9A58h para DPTR
-	NOP               ; ciclo ocioso
-	SJMP $            ; pula para a mesma instrucao
+	MOV  ACC,  #0Ah   ; move 0Ah diretamente para ACC            ; 2 us
+	MOV  ACC,  #00h   ; move 00h diretamente para ACC            ; 2 us
+	MOV  R0,   #0Bh   ; move 0Bh para R0 do banco 0              ; 1 us
+	MOV  B,    #0Ch   ; move 0Ch para o registrador C            ; 2 us
+	MOV  7Fh,  P1     ; move a porta P1 para o enderco 7Fh       ; 2 us
+	SETB PSW.3        ; seleciona o banco 1                      ; 1 us
+	MOV  R0,   7Fh    ; escreve conteudo de 7Fh em R0 do banco 1 ; 2 us
+	MOV  7Eh,  R0     ; escreve R0 (banco 1) em 7Eh              ; 2 us
+	MOV  R1,   #7Eh   ; R1 ponteiro para 7Eh                     ; 1 us
+	MOV  ACC,  @R1    ; move conteudo no endereco R1 para ACC    ; 2 us
+	MOV  DPTR, #9A5Bh ; move 9A58h para DPTR                     ; 2 us
+	NOP               ; ciclo ocioso                             ; 1 us
+	SJMP $            ; pula para a mesma instrucao              ; 2 us
---
+++ 8051/Readme.md
@@ -0,0 +1,17 @@
+# Respostas das Questões
+## Exercício 1
+(a) O tempo de cada instrução está especificado nos comentários do código "Exercise1.asm". O tempo total de execução, descontando o tempo da instrução final "SJMP $" foi de 20 us.
+
+(b) Cada ciclo de máquina do 8051 dura 1 us, isso significa que o programa, descontando a instrução final "SJMP $", possui 20 ciclos de máquina.
+
+(c) Os valores dos cada um dos 8 bits de P1 foram passados para ACC como um valor de 2 bytes. Esse valor foi FFh, pois os Ports 0-3 do 8051 são automaticamente inicializados para o valor binário 11111111b.
+
+(d) O valor que ACC assumiu ao passar R1 indiretamente para ele foi o que havia contido no endereço 7Eh, que, nesse caso, era FFh.
+
+(e) Foi possível mover um valor de 4 digitos hexadecimais para DPTR, pois DPTR é composto de 2 outros registradores de 8 bits, DPH e DPL, portanto 2 digitos foram colocados em DPH e 2 outros em DPL. Dito isso, o maior valor que pode ser passado para DPTR é FFFFh.
+
+## Exercício 2
+
+## Exercício 3
+
+## Exercício 4
